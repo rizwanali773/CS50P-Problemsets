f262ff111c1d8f475b1f60bd2068713a8430f4c3
rizwanali773 <167829500+rizwanali773@users.noreply.github.com>
2026-04-02 01:00:40 +0530

    function added

--- Week 1/interpreter/interpreter.py
+++ Week 1/interpreter/interpreter.py
@@ -1,15 +1,17 @@
-expression = input("Expression: ")
-x, y, z = expression.split(" ")
-x , z = int(x), float(z)
-if y == '+':
-    print(x + z)
-elif y == '-':
-    print(x - z)
-elif y == '/':
-    result = x / z
-    print(f"{result:.1f}")
-elif y == '*':
-    print(x * z)
-else:
-    print("Expresion is undefined")
+def main():
+    expression = input("Expression: ")
+    x, y, z = expression.split(" ")
+    x , z = int(x), float(z)
+    if y == '+':
+        print(x + z)
+    elif y == '-':
+        print(x - z)
+    elif y == '/':
+        result = x / z
+        print(f"{result:.1f}")
+    elif y == '*':
+        print(x * z)
+    else:
+        print("Expresion is undefined")
 
+main()
